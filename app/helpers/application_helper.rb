84c8556d655853cbc10c1bf9117c2920c9da9179
Kyohei <kiwai@coe.edu>
2014-12-01 15:55:53 +0900

    heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    base_title = "歌舞伎妖精"
+    base_title = "Shinoe & Iwai"
     if page_title.empty?
       base_title
     else
