bd1d0eca37c7cd0ae02bc3a53b20da6ae4f32097
yuxing <yuxing0129@gmail.com>
2015-02-02 22:41:28 +0900

    modify index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    base_title = "Shinoe & Iwai"
+    base_title = "Lang Block"
     if page_title.empty?
       base_title
     else
