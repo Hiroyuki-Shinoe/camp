b99c7e5e9627f962c28bb138137a5f7b39f33dd4
yuxing <yuxing0129@gmail.com>
2015-08-06 18:24:43 +0900

    aws

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -2,8 +2,8 @@ if defined?(AssetSync)
   AssetSync.configure do |config|
     config.fog_provider = 'AWS'
     config.fog_directory = ENV['langblock']
-    config.aws_access_key_id = ENV['AKIAJGNO555DMDTARN3A']
-    config.aws_secret_access_key = ENV['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']
+    config.aws_access_key_id = ENV['']
+    config.aws_secret_access_key = ENV['']
     config.manifest = true
     config.fog_region = ENV['ap-northeast-1']
   end
